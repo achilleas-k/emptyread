c4fc7043e6670f4f433c1e2d4d37ec86c500c3e5
Achilleas Koutsou <achilleas.k@gmail.com>
2017-06-03 14:25:35 +0200

    Numpy only

--- read.py
+++ read.py
@@ -1,21 +1,9 @@
-import h5py
 import numpy as np
 
-
-hfile = h5py.File("emptyread.h5", mode='w')
-grp = hfile.create_group("grp")
-ds = grp.create_dataset("data", data=np.random.random((3, 10, 5)))
 
 for idx in range(200):
     print(idx)
     emptythingie = np.empty((1,))
-    sl = (slice(1, 2, None), slice(1, 2, None), slice(0, 1, None))
-    print(sl)
-    print(ds.shape)
-    print(emptythingie.shape)
-    # uncommenting below probably makes it succeed
-    print(ds[:])
-    ds.read_direct(emptythingie, sl)
     emptythingie.resize(())
 
-hfile.close()
+print("DONE")

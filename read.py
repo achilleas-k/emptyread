9e7644d7e7991af3906c824157d33390633f1375
Achilleas Koutsou <achilleas.k@gmail.com>
2017-06-03 13:46:18 +0200

    Lotsa printing

--- read.py
+++ read.py
@@ -9,7 +9,11 @@ ds = grp.create_dataset("data", data=np.random.random((3, 10, 5)))
 for idx in range(200):
     print(idx)
     emptythingie = np.empty((1,))
-    ds.read_direct(emptythingie,
-                   (slice(1, 2, None), slice(1, 2, None), slice(0, 1, None)))
+    sl = (slice(1, 2, None), slice(1, 2, None), slice(0, 1, None))
+    print(sl)
+    print(ds.shape)
+    print(emptythingie.shape)
+    print(ds[:])
+    ds.read_direct(emptythingie, sl)
 
 hfile.close()

e8bbf53d75f1770a1e1a09abd819e25e4c087ae9
Achilleas Koutsou <achilleas.k@gmail.com>
2017-06-03 13:34:46 +0200

    Try again

--- read.py
+++ read.py
@@ -4,11 +4,11 @@ import numpy as np
 
 hfile = h5py.File("emptyread.h5", mode='w')
 grp = hfile.create_group("grp")
-ds = grp.create_dataset("data", data=np.random.random((10, 5, 10)))
+ds = grp.create_dataset("data", data=np.random.random((3, 10, 5)))
 
 for idx in range(200):
     print(idx)
-    emptythingie = np.empty([])
+    emptythingie = np.empty((1,))
     ds.read_direct(emptythingie,
                    (slice(1, 2, None), slice(1, 2, None), slice(0, 1, None)))
 
